b8317e2c2e0f0a3b260c4eac50c5ddebe0b20741
Mael REINERT MARTINEZ <mael@reinertmartinez.fr>
2025-02-23 13:41:43 +0100

    Envoyer un mail de bienvenue

--- server/manifest.js
+++ server/manifest.js
@@ -51,7 +51,7 @@ module.exports = new Confidence.Store({
                                 user     : process.env.DB_USER || 'root',
                                 password : process.env.DB_PASSWORD || 'hapi',
                                 database : process.env.DB_DATABASE || 'user',
-                                port     : process.env.DB_PORT || 3307
+                                port     : process.env.DB_PORT || 3306
                             }
                         }
                     },
